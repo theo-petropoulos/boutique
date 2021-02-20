134b6f14b342974aba8d19cbda45a5b41020cdf5
adam-fraga <adam.fraga@laplateforme.io>
2021-02-20 22:38:45 +0100

    Gestion dynamique collections et produits

--- pages/collection_audemars.php
+++ pages/collection_audemars.php
@@ -1,5 +1,22 @@
-<?php ?>
+<?php require_once '../model/class/Manager.php' ?>
 
+<?php
+$_GET['collection'] = 3;
+
+$man = new \App\Manager();
+$marques = $man->getCollection();
+if (isset($_GET['collection']) && $_GET['collection'] == 1) {
+    $produits = $man->getProductByCollection(1);
+} elseif (isset($_GET['collection']) && $_GET['collection'] == 2) {
+    $produits = $man->getProductByCollection(2);
+} elseif
+(isset($_GET['collection']) && $_GET['collection'] == 3) {
+    $produits = $man->getProductByCollection(3);
+} else {
+    $produits = $man->getProductByCollection(1);
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,67 +44,79 @@
             <!--Section produits-->
             <section class="container_products">
                 <div class="container_product">
-                    <img class="product_pic" src="../assets/images/product_audemars/AP1.jpg" alt="Montre Audemars Piguet">
-                    <h4 class="product_title">Product name</h4>
+                    <img class="product_pic" src="../assets/images/product_audemars/<?= $produits[0]['image'] ?>"
+                         alt="Montre Audemars Piguet">
+                    <h4 class="product_title"><?= $produits[0]['nom'] ?></h4>
                     <p class="p_product">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, sit!
                     </p>
-                    <span><?= "PRIX" ?></span>
+                    <span><?= $produits[0]['prix'] . '€' ?></span>
 
-                    <button class="buy"><i class="far fa-plus-square"></i>Ajouter</button>
+                    <a href="produit.php?produit=<?= $produits[0]['id'] ?>>&collection=<?= $marques[0]['id'] ?>"
+                       class="buy"><i class="far fa-plus-square"></i>Ajouter</a>
                 </div>
                 <div class="container_product">
-                    <img class="product_pic" src="../assets/images/product_audemars/AP2.jpg" alt="Montre Audemars Piguet">
-                    <h4 class="product_title">Product name</h4>
+                    <img class="product_pic" src="../assets/images/product_audemars/<?= $produits[1]['image'] ?>"
+                         alt="Montre Audemars Piguet">
+                    <h4 class="product_title"><?= $produits[1]['nom'] ?></h4>
                     <p class="p_product">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, quidem?
                     </p>
-                    <span><?= "PRIX" ?></span>
+                    <span><?= $produits[1]['prix'] . '€' ?></span>
 
-                    <button class="buy"><i class="far fa-plus-square"></i>Ajouter</button>
+                    <a href="produit.php?produit=<?= $produits[1]['id'] ?>>&collection=<?= $marques[0]['id'] ?>"
+                       class="buy"><i class="far fa-plus-square"></i>Ajouter</a>
                 </div>
                 <div class="container_product">
-                    <img class="product_pic" src="../assets/images/product_audemars/AP3.jpg" alt="Montre Audemars Piguet">
-                    <h4 class="product_title">Product name</h4>
+                    <img class="product_pic" src="../assets/images/product_audemars/<?= $produits[2]['image'] ?>"
+                         alt="Montre Audemars Piguet">
+                    <h4 class="product_title"><?= $produits[2]['nom'] ?></h4>
                     <p class="p_product">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, nostrum?
                     </p>
-                    <span><?= "PRIX" ?></span>
-                    <button class="buy"><i class="far fa-plus-square"></i>Ajouter</button>
+                    <span><?= $produits[2]['prix'] . '€' ?></span>
+                    <a href="produit.php?produit=<?= $produits[2]['id'] ?>>&collection=<?= $marques[0]['id'] ?>"
+                       class="buy"><i class="far fa-plus-square"></i>Ajouter</a>
                 </div>
                 <div class="container_product">
-                    <img class="product_pic" src="../assets/images/product_audemars/AP5.jpg" alt="Montre Audemars Piguet">
-                    <h4 class="product_title">Product name</h4>
+                    <img class="product_pic" src="../assets/images/product_audemars/<?= $produits[3]['image'] ?>"
+                         alt="Montre Audemars Piguet">
+                    <h4 class="product_title"><?= $produits[3]['nom'] ?></h4>
                     <p class="p_product">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, numquam!
 
                     </p>
-                    <span><?= "PRIX" ?></span>
+                    <span><?= $produits[3]['prix'] . '€' ?></span>
 
-                    <button class="buy"><i class="far fa-plus-square"></i>Ajouter</button>
+                    <a href="produit.php?produit=<?= $produits[3]['id'] ?>>&collection=<?= $marques[0]['id'] ?>"
+                       class="buy"><i class="far fa-plus-square"></i>Ajouter</a>
                 </div>
                 <div class="container_product">
-                    <img class="product_pic" src="../assets/images/product_audemars/AP4.jpg" alt="Montre Audemars Piguet">
-                    <h4 class="product_title">Product name</h4>
+                    <img class="product_pic" src="../assets/images/product_audemars/<?= $produits[4]['image'] ?>"
+                         alt="Montre Audemars Piguet">
+                    <h4 class="product_title"><?= $produits[4]['nom'] ?></h4>
                     <p class="p_product">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, quia.
 
                     </p>
-                    <span><?= "PRIX" ?></span>
+                    <span><?= $produits[4]['prix'] . '€' ?></span>
 
-                    <button class="buy"><i class="far fa-plus-square"></i>Ajouter</button>
+                    <a href="produit.php?produit=<?= $produits[4]['id'] ?>>&collection=<?= $marques[0]['id'] ?>"
+                       class="buy"><i class="far fa-plus-square"></i>Ajouter</a>
                 </div>
                 <div class="container_product">
 
-                    <img class="product_pic" src="../assets/images/product_audemars/AP6.jpg" alt="Montre Audemars Piguet">
-                    <h4 class="product_title">Product name</h4>
+                    <img class="product_pic" src="../assets/images/product_audemars/<?= $produits[5]['image'] ?>"
+                         alt="Montre Audemars Piguet">
+                    <h4 class="product_title"><?= $produits[5]['nom'] . '€' ?></h4>
                     <p class="p_product">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, nisi.
 
                     </p>
-                    <span><?= "PRIX" ?></span>
+                    <span><?= $produits[5]['prix'] . '€' ?></span>
 
-                    <button class="buy"><i class="far fa-plus-square"></i>Ajouter</button>
+                    <a href="produit.php?produit=<?= $produits[5]['id'] ?>>&collection=<?= $marques[0]['id'] ?>"
+                       class="buy"><i class="far fa-plus-square"></i>Ajouter</a>
                 </div>
             </section>
             <!--Section description de la gamme -->
