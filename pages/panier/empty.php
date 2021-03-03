26850aae3719c3c3e7a5eea44fdab220f0f96f87
Theo <theo.petropoulos@laplateforme.io>
2021-03-03 18:42:26 +0100

    fixes

--- pages/panier/empty.php
+++ pages/panier/empty.php
@@ -2,7 +2,7 @@
     <section id="block_haut">
         <div class="text">Votre panier est vide.</div>
     </section>
-    <section id="suggestions_basket">
-        <?php require realpath($_SERVER["DOCUMENT_ROOT"]) . '/boutique/pages/panier/suggestions.php';?>
+    <section class="suggestions">
+        <?php $sugg_qty=5; require realpath($_SERVER["DOCUMENT_ROOT"]) . '/boutique/pages/panier/suggestions.php';?>
     </section>
 </main>
--- pages/panier/suggestions.php
+++ pages/panier/suggestions.php
@@ -6,36 +6,34 @@
     $query=$db->query('SELECT * FROM `produits`');
     $items=$query->fetchAll(PDO::FETCH_ASSOC);
 
-    $amount=count($items)-1;
-    $rand=rand(0,$amount);
-    $i=0;
-    foreach($items as $key=>$value){
-        if($rand==$i){
-            $sugg=$value;
-            $i++;
+    for ($i=0;$i<$sugg_qty;$i++){
+        $amount=count($items)-1;
+        $rand=rand(0,$amount);
+        $j=0;
+        
+        foreach($items as $key=>$value){
+            if($rand==$j){
+                $sugg=$value;
+                unset($items[$key]);
+                break;
+            }
+            else $j++;
         }
-        else $i++;
-    }
-    $stmt=$db->prepare('SELECT * FROM `marques` WHERE `id`=?');
-    $stmt->execute([$sugg['id_marque']]);
-    $marque=$stmt->fetch(PDO::FETCH_ASSOC);
 
-    if(isset($marque['nom']) && $marque['nom']){
-        if($marque['nom']=='Audemars Piguet'){
-            $nom='Audemars';
-        }
-        else $nom=$marque['nom'];
-    }
-    $url='/boutique/assets/images/product_'.$nom.'/'.$sugg['image'];
-    ?>
+        $stmt=$db->prepare('SELECT * FROM `marques` WHERE `id`=?');
+        $stmt->execute([$sugg['id_marque']]);
+        $marque=$stmt->fetch(PDO::FETCH_ASSOC);
 
-    <div class="sugg_product">
-        <img src='<?=$url;?>'>
-        <p><?=$marque['nom'];?></p>
-        <p><?=$sugg['nom'];?></p>
-        <p><?=$sugg['prix'];?> €</p>
-        <form method="post" action="">
-            <input type="hidden" name="addbasket" value="<?=$sugg['id'];?>">
-            <input type="submit" value="Ajouter au panier">
-        </form>
-    </div>
+        $url='/boutique/assets/images/produits/'.$sugg['image'];?>
+
+        <div class="sugg_product">
+            <img src='<?=$url;?>'>
+            <p><?=$marque['nom'];?></p>
+            <p><?=$sugg['nom'];?></p>
+            <p><?=$sugg['prix'];?> €</p>
+            <form method="post" action="">
+                <input type="hidden" name="addbasket" value="<?=$sugg['id'];?>">
+                <input type="submit" value="Ajouter au panier">
+            </form>
+        </div>
+    <?php } ?>
