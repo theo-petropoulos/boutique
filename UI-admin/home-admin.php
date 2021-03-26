252ccdb2a18e7a29f5196bfba6f7ce7b1f3c444b
adam-fraga <adam.fraga@laplateforme.io>
2021-03-26 19:47:12 +0100

    refonte boutton admin

---
+++ UI-admin/home-admin.php
@@ -0,0 +1,13 @@
+<main>
+    <section class="home_admin form_manage" >
+        <h1 class="title_home_admin">Bienvenue sur votre espace d'administration</h1>
+        <div><i class="fas fa-user-shield"></i></div>
+        <p class="p_home_admin">La pannel d'administration vous permet de gérer à la fois votre boutique mais également les informations de vos clients.
+            Vous souhaitez ajouter des produits à votre catalogue? une collection en particulier? ou recueillir des
+            information concernant les commandes d'un client?
+            Rien de plus simple commencer par vous connecter en cliquer sur le lien ci dessous et gérez en toute
+            simplicité votre E-buisness!
+        </p>
+        <a href="?path=connect-admin" id="btn_conn_admin" class="link_admin">Connexion</a>
+    </section>
+</main>
