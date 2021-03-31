1099db9023d66320bce72667cd3ce598ccc30153
Theo <theo.petropoulos@laplateforme.io>
2021-03-31 08:14:49 +0200

    minor fixes

--- pages/globals/head.php
+++ pages/globals/head.php
@@ -7,7 +7,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Beth+Ellen&family=Bodoni+Moda&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css2?family=Rosario&display=swap" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css2?family=Poiret+One&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Poiret+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/9ddb75d515.js" crossorigin="anonymous"></script>
     <title><?=$title;?></title>
 </head>
